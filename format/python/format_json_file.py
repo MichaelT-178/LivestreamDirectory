9d3217011cbe0d16b741616542ddfeb776f90e8c
Michael Totaro <mtotaro@ncsu.edu>
2023-02-03 21:58:47 -0500

    Adding files

--- format/python/format_json_file.py
+++ format/python/format_json_file.py
@@ -41,7 +41,7 @@ def youtube_links(video_id, time):
     t = [int(x) for x in time.split(":")]
     seconds = t[1] + (t[0] * 60) if (len(t) == 2) else t[2] + (t[1] * 60) + (t[0] * 3600)
 
-    if ("share" in video_id): #Youtube updated their links.
+    if ("share" in video_id): #Youtube updated their livestream links. 2/3/23
         return f"https://www.youtube.com/live/{video_id}&t={seconds} , " 
 
     return f"https://youtu.be/{video_id}?t={seconds} , " 
