f4f27574c29689e1cd05ffd4589db2e819fbe30c
Michael Totaro <mtotaro@ncsu.edu>
2024-01-01 11:45:04 -0500

    Updated OperartingSystem.cs

--- db_manager/main_algorithm/OperatingSystem.cs
+++ db_manager/main_algorithm/OperatingSystem.cs
@@ -6,7 +6,8 @@ using System.Diagnostics;
  *
  * Methods
  * OpenFileInVSCode | Opens a file in VSCode for viewing.
- * ExecuteGitCommands | Adds, commits, and pushes project to Github
+ * ExecuteGitCommands | Adds, commits, and pushes project to 
+ * ExecuteCommand | Executes a command on the operating system.
  *
  * @author Michael Totaro
  */
@@ -46,8 +47,44 @@ class OS {
      * Executes the git commands git add, git commit, and git push commands. 
      */
     public static void ExecuteGitCommands()
+    {   
+        ExecuteCommand("git add .");
+        Color.PrintLine("git add completed successfully", "Green");
+
+        ExecuteCommand("git commit -m \"Adding files\"");
+        Color.PrintLine("git commit completed successfully", "Green");
+
+        ExecuteCommand("git push");
+        Color.PrintLine("git push completed successfully", "Green");
+    }
+
+    /**
+     * Executes a command on the operating system.
+     * @param command to be executed.
+     */
+    public static void ExecuteCommand(string command)
     {
-        
+        ProcessStartInfo processStartInfo = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true,
+            UseShellExecute = false
+        };
+
+        Process process = new Process
+        {
+            StartInfo = processStartInfo
+        };
+
+        process.Start();
+
+        process.StandardInput.WriteLine(command);
+        process.StandardInput.Flush();
+        process.StandardInput.Close();
+
+        process.WaitForExit();
     }
 
 }
