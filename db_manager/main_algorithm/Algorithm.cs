7df50ecd8fb70a7d51f9f28437f5c09e96da0964
Michael Totaro <mtotaro@ncsu.edu>
2024-01-01 00:31:02 -0500

    Documented Algorithm.cs

--- db_manager/main_algorithm/Algorithm.cs
+++ db_manager/main_algorithm/Algorithm.cs
@@ -1,7 +1,24 @@
+/**
+ *
+ * This class runs the algorithm to convert all-timestamps.txt
+ * to song_list.json in the database directory.
+ *
+ * Methods
+ * run | Runs the algorithm
+ *
+ * @author Michael Totaro
+ */
 class Algorithm
-{
+{   
+    /**
+     * This algorithm creates the song_list.json database from 
+     * the allSongs string list and all-timestamps.txt file.
+     * @param allSongs All songs from all-timestamps.txt. Each song
+     *        only appears one time and has been stripped of it's keys.
+     */
     public static void run(List<string> allSongs)
     {
         
     }
+
 }
