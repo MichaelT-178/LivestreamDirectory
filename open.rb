25c3b01cbec62ae93a9bae366f1ed1080e365a0d
Michael Totaro <mtotaro@ncsu.edu>
2024-01-06 00:17:43 -0500

    Adding files

--- open.rb
+++ open.rb
@@ -1,2 +1,7 @@
+=begin 
+This program simply opens the LivestreamDirectory website in the browser 
+when run.
+=end
+
 require 'launchy'
 Launchy.open("https://michaelt-178.github.io/LivestreamDirectory/")
