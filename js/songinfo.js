27782c0f41748b57dc868c51dc1a49214f4713a6
Michael Totaro <mtotaro@ncsu.edu>
2024-01-25 21:44:28 -0500

    Added changes

--- js/songinfo.js
+++ js/songinfo.js
@@ -28,7 +28,7 @@ window.addEventListener('load', () => {
     } else {
         if (theTitle.length < 30 && artist.length < 30 && other_artists.length < 30 && longestString < 30) {
             const songInfoHeading = document.getElementById('songInfo');
-            const newMarginLeft = songInfoHeading.style.marginLeft - 120;
+            const newMarginLeft = songInfoHeading.style.marginLeft - 40;
 
             songInfoHeading.style.marginLeft = newMarginLeft + 'px';
         } 
