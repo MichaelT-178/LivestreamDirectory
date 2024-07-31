a32f33a9a1075aa48b33cd22856ee0e68370a786
Michael Totaro <mstotaro@gmail.com>
2024-07-31 17:24:52 -0400

    Adding changes

---
+++ open.py
@@ -0,0 +1,8 @@
+"""
+This program simply opens the LivestreamDirectory website in the browser 
+when you run it.
+"""
+
+import webbrowser
+
+webbrowser.open("https://michaelt-178.github.io/LivestreamDirectory/")
--- open.rb
+++
@@ -1,7 +0,0 @@
-=begin 
-This program simply opens the LivestreamDirectory website in the browser 
-when run.
-=end
-
-require 'launchy'
-Launchy.open("https://michaelt-178.github.io/LivestreamDirectory/")
