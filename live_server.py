dbb2992ecf0d18fcd7611e5662083011d8732546
Michael Totaro <mstotaro@gmail.com>
2025-01-16 09:40:50 -0500

    Updated live server

--- live_server.py
+++ live_server.py
@@ -1,4 +1,13 @@
+import subprocess
 from livereload import Server
+
+def write_to_clipboard():
+    output = 'http://localhost:8000'
+    process = subprocess.Popen('pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
+    process.communicate(output.encode('utf-8'))
+    print("\nhttp://localhost:8000 was copied to clipboard!\n")
+
+write_to_clipboard()
 
 server = Server()
 
