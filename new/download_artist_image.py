c53481d2cadda598e8498725c98d208d4e3de1b4
Michael Totaro <mstotaro@gmail.com>
2025-06-14 23:44:25 -0400

    Updated download_artist_image.py

--- new/download_artist_image.py
+++ new/download_artist_image.py
@@ -1,8 +1,14 @@
 import os
+import subprocess
 from spotify_images import SpotifyApi
 from clean_text import clean_text
 from push_to_vue import push_to_vue
 from termcolor import colored as c
+
+def write_to_clipboard(output: str):
+	process = subprocess.Popen('pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
+	process.communicate(output.encode('utf-8'))
+
 
 artist = input("Enter the artist name: ")
 
@@ -23,6 +29,25 @@ file_path = f'../pics/{clean_artist_name}.jpg'
 if not os.path.isfile(file_path):
     os.system(f'cp ../../VueLivestreamDirectory/src/assets/ArtistPics/{clean_artist_name}.jpg ../pics/')
     print(c('\n\nCopied new image to the pics folder!', 'green'))
+
+
+
+    artist_output = f'''"{clean_artist_name}": {{
+        "Artist": "{artist.strip()}",
+        "CleanedArtist": "{clean_artist_name}",
+        "Location": "",
+        "YearFormed": 0,
+        "Genre": ""
+    }}'''
+
+    print("\n")
+    print(artist_output)
+    print()
+
+    print(c("Artist object copied to clipboard!", 'green'))
+    print(c("Go paste this in 'db_manager/json_files/artists.json'", 'green'))
+
+    write_to_clipboard(artist_output)
 else:
     print(c('\n\nThe image already exists in the pics folder!', 'green'))
 
